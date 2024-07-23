d98b1c8b0dc0cae78f2fb74fc900bcea249281a3
Nguyen Hong Hai <hainhph35304@fpt.edu.vn>
2024-07-23 21:46:18 +0700

    change view

--- View/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace View.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- View/Program.cs
+++ View/Program.cs
@@ -1,3 +1,5 @@
+using View.Components;
+
 namespace View
 {
     public class Program
@@ -7,28 +9,26 @@ namespace View
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
+
             app.UseStaticFiles();
+            app.UseAntiforgery();
 
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
 
             app.Run();
         }
