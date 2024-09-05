d23e57e4b784e42a3ddbb71da751ddb4c2a446d5
IamBemooooo <nguyendinhthe405@gmail.com>
2024-09-05 11:13:10 +0700

    Add template,Add Js global,Add fontAwesome

---
+++ View/Controllers/PhotoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace View.Controllers
+{
+	public class PhotoController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
--- View/Program.cs
+++ View/Program.cs
@@ -1,40 +1,36 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using MudBlazor.Services;
-
 namespace View
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddRazorPages();
-            builder.Services.AddServerSideBlazor();
-            builder.Services.AddMudServices();
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
 
-            var app = builder.Build();
+			var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
 
-            app.UseHttpsRedirection();
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
 
-            app.UseStaticFiles();
+			app.UseRouting();
 
-            app.UseRouting();
+			app.UseAuthorization();
 
-            app.MapBlazorHub();
-            app.MapFallbackToPage("/_Host");
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
 
-            app.Run();
-        }
-    }
+			app.Run();
+		}
+	}
 }
