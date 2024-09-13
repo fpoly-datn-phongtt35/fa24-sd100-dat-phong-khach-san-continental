0666a0516c216af879fd812c53f5c9291d24684e
IamBemooooo <nguyendinhthe405@gmail.com>
2024-09-13 09:49:03 +0700

    add EmailHelpers

---
+++ Domain/Services/IServices/IEmailHelpers.cs
@@ -0,0 +1,14 @@
+﻿using Domain.DTO.Email;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services.IServices
+{
+    public interface IEmailHelpers
+    {
+        Task<bool> SeedGmail(SeedMailRequest request, string mailName, string appPass);
+    }
+}
