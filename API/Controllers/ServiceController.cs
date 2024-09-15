a015d5e37473d0718b0925b4a0ec53091e813662
Vũ Tuấn Anh <tuananh26ttvt@gmail.com>
2024-09-15 15:50:54 +0700

    service

---
+++ API/Controllers/ServiceController.cs
@@ -0,0 +1,88 @@
+﻿using Domain.DTO.Paging;
+using Domain.DTO.Service;
+using Domain.DTO.ServiceType;
+using Domain.Models;
+using Domain.Services.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        private readonly IServiceService _serviceSV;
+
+        public ServiceController(IServiceService serviceSV)
+        {
+            _serviceSV = serviceSV;
+        }
+
+        [HttpPost("CreateService")]
+        public async Task<int> CreateServiceType(ServiceCreateRequest request)
+        {
+            try
+            {
+                return await _serviceSV.AddService(request);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPost("GetListService")]
+        public async Task<ResponseData<Service>> GetListService(ServiceGetRequest request)
+        {
+            try
+            {
+                return await _serviceSV.GetServices(request);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPost("GetServiceById")]
+        public async Task<Service> GetServiceById(Guid Id)
+        {
+            try
+            {
+                return await _serviceSV.GetServiceById(Id);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPut("UpdateService")]
+        public async Task<int> UpdateService(ServiceUpdateRequest request)
+        {
+            try
+            {
+                return await _serviceSV.UpdateService(request);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpDelete("DeleteService")]
+        public async Task<IActionResult> DeleteService([FromBody] ServiceDeleteRequest request)
+        {
+            try
+            {
+                int result = await _serviceSV.DeleteService(request);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Message = "Error deleting service", Details = ex.Message });
+            }
+        }
+    }
+}
---
+++ Domain/DTO/Service/ServiceDeleteRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTO.Service
+{
+    public class ServiceDeleteRequest
+    {
+        public Guid Id { get; set; }
+        public Guid? DeletedBy { get; set; }
+        public DateTimeOffset DeletedTime { get; set; }
+    }
+}
