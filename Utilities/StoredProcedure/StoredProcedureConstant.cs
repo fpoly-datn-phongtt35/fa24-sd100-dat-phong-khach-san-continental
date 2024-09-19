3dd79d04374d142a501121f29b3d3d015977bf90
Vũ Tuấn Anh <tuananh26ttvt@gmail.com>
2024-09-19 18:59:24 +0700

    save before merge

--- Utilities/StoredProcedure/StoredProcedureConstant.cs
+++ Utilities/StoredProcedure/StoredProcedureConstant.cs
@@ -22,17 +22,14 @@ namespace Utilities.StoredProcedure
         public const string SP_DeleteService = "SP_DeleteService";
         public const string SP_GetServiceById = "SP_GetServiceById";
         public const string SP_UpdateService = "SP_UpdateService";
-<<<<<<< HEAD
         public const string SP_GetServiceByTypeId = "PP_GetServiceByTypeId";
         
 
-=======
         //building
         public const string SP_InsertBuilding = "SP_InsertBuilding";
         public const string SP_GetListBuilding = "SP_GetListBuilding";
         public const string SP_DeleteBuilding = "SP_DeleteBuilding";
         public const string SP_GetBuildingById = "SP_GetBuildingById";
         public const string SP_UpdateBuilding = "SP_UpdateBuilding";
->>>>>>> 7295a98dd660a4ff4677ea664365f927e1058ac4
     }
 }
