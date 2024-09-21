1b0cc6e3914fb93a19bfd35d927cca84f1d097df
Nguyen Hong Hai <hainhph35304@fpt.edu.vn>
2024-09-22 01:52:11 +0700

    CustomerAPI, delete Name from ServiceOrderDetail, fix conflict

---
+++ Utilities/PasswordHashingHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace Utilities
+{
+    public static class PasswordHashingHelper
+    {
+        public static string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return Convert.ToBase64String(bytes);
+            }
+        }
+    }
+}
