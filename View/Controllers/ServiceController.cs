b9cb16e241200cfaaca03b49a1d50b4066b565e3
Vũ Tuấn Anh <tuananh26ttvt@gmail.com>
2024-09-29 21:12:43 +0700

    giao diện CRUD Service

---
+++ View/Controllers/ServiceController.cs
@@ -0,0 +1,194 @@
+﻿using Domain.DTO.Paging;
+using Domain.DTO.Service;
+using Domain.DTO.ServiceType;
+using Domain.Enums;
+using Domain.Models;
+using Domain.Services.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using View.Models;
+
+namespace View.Controllers
+{
+    public class ServiceController : Controller
+    {
+        HttpClient _client;
+
+        public ServiceController(HttpClient client)
+        {
+            _client = client;
+            _client.BaseAddress = new Uri("https://localhost:7130/");
+        }
+
+        // GET: ServiceController
+        public async Task<IActionResult> Index()
+        {
+            // api url
+            string requestUrl = "https://localhost:7130/api/Service/GetListService";
+
+            var request = new ServiceGetRequest
+            {
+                PageIndex = 1,  
+                PageSize = 10
+            };
+
+            // comvert request to json
+            var jsonRequest = JsonConvert.SerializeObject(request);
+
+            var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+            try
+            {
+                // send request den api
+                var response = await _client.PostAsync(requestUrl, content);
+
+                // doc ket qua tu api
+                var responseString = await response.Content.ReadAsStringAsync();
+
+                // convert json to respondata 
+                var services = JsonConvert.DeserializeObject<ResponseData<Service>>(responseString);
+
+                return View(services);
+            }
+            catch (Exception ex)
+            {
+                return View("Error", ex);
+            }
+        }
+
+
+        // GET: ServiceController/Details/5
+        public async Task<IActionResult> Details(Guid id)
+        {
+            string requestUrl = $"https://localhost:7130/api/Service/GetServiceById?id={id}";
+
+            // Tạo nội dung json cho request
+            var jsonRequest = JsonConvert.SerializeObject(new { Id = id });
+            var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+            try
+            {
+                var response = await _client.PostAsync(requestUrl, content);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return View("Error");
+                }
+
+                var responseString = await response.Content.ReadAsStringAsync();
+                var services = JsonConvert.DeserializeObject<Service>(responseString);
+
+
+
+                return View(services);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+
+
+
+
+
+
+        // GET: ServiceController/Create
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Units = Enum.GetValues(typeof(UnitType));
+            return View(new ServiceCreateRequest());
+        }
+
+        // POST: ServiceController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ServiceCreateRequest request)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _client.PostAsJsonAsync("api/Service/CreateService", request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            ViewBag.Units = Enum.GetValues(typeof(UnitType));
+
+            return View(request);
+        }
+
+        // GET: ServiceController/Edit/5
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            string requestUrl = $"https://localhost:7130/api/Service/GetServiceById?id={id}";
+
+            var jsonRequest = JsonConvert.SerializeObject(new { Id = id });
+            var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+            ViewBag.Units = Enum.GetValues(typeof(UnitType));
+            ViewBag.Statuses = Enum.GetValues(typeof(EntityStatus));
+
+            try
+            {
+                var response = await _client.PostAsync(requestUrl, content);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return View("Error");
+                }
+
+                var responseString = await response.Content.ReadAsStringAsync();
+                var services = JsonConvert.DeserializeObject<Service>(responseString);
+
+
+
+                return View(services);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        // POST: ServiceController/Edit/5
+        [HttpPost]
+        public async Task<IActionResult> Edit(Service request)
+        {
+            ViewBag.Units = Enum.GetValues(typeof(UnitType));
+            ViewBag.Statuses = Enum.GetValues(typeof(EntityStatus));
+
+            var response = await _client.PutAsJsonAsync("api/Service/UpdateService", request);
+            return RedirectToAction("Index"); 
+        }
+
+
+        // DELETE: ServiceController/Delete/5
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            string requestUrl = "https://localhost:7130/api/Service/DeleteService";
+
+            var request = new ServiceDeleteRequest
+            {
+                Id = id,
+                DeletedBy = Guid.NewGuid(),  //tạm thời, sau lấy giá trị từ người dùng đang đăng nhập
+                DeletedTime = DateTimeOffset.Now
+            };
+
+            var jsonRequest = JsonConvert.SerializeObject(request);
+            var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync(requestUrl, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Error", new Exception("Unable to delete the service."));
+        }
+    }
+}
---
+++ View/Controllers/ServiceOrderController.cs
@@ -0,0 +1,157 @@
+﻿using Domain.DTO.Paging;
+using Domain.DTO.ServiceOrder;
+using Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace View.Controllers
+{
+    public class ServiceOrderController : Controller
+    {
+        HttpClient _client;
+
+        public ServiceOrderController(HttpClient client)
+        {
+            _client = client;
+            _client.BaseAddress = new Uri("https://localhost:7130/");
+        }
+        // GET: ServiceOrderController
+        public async Task<IActionResult> Index()
+        {
+            string requestUrl = "https://localhost:7130/api/ServiceOrder/GetListServiceOrders";
+
+            var request = new ServiceOrderGetRequest
+            {
+                PageIndex = 1,
+                PageSize = 10
+                ,RoomBookingDetailId = null
+            };
+
+            var jsonRequest = JsonConvert.SerializeObject(request);
+            
+            var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+            try
+            {
+                var response = await _client.PostAsync(requestUrl, content);
+
+                var responseString = await response.Content.ReadAsStringAsync();
+
+                var serviceOrders = JsonConvert.DeserializeObject<ResponseData<ServiceOrder>>(responseString);
+
+                if (serviceOrders == null || serviceOrders.data == null)
+                {
+                    serviceOrders = new ResponseData<ServiceOrder>
+                    {
+                        data = new List<ServiceOrder>(),
+                        totalPage = 0,
+                        totalRecord = 0,
+                        CurrentPage = 1,
+                        PageSize = 10
+                    };
+                }
+
+                return View(serviceOrders);
+            }
+            catch (Exception ex)
+            {
+                return View("Error", ex);
+            }
+        }
+
+        // GET: ServiceOrderController/Details/5
+        public async Task<IActionResult> Details(Guid id)
+        {
+            string requestUrl = $"https://localhost:7130/api/ServiceOrder/GetServiceOrderById?id={id}";
+
+            var jsonRequest = JsonConvert.SerializeObject(new { Id = id });
+            var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+            try
+            {
+                var response = await _client.PostAsync(requestUrl, content);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return View("Error");
+                }
+
+                var responseString = await response.Content.ReadAsStringAsync();
+                var services = JsonConvert.DeserializeObject<ServiceOrder>(responseString);
+
+
+
+                return View(services);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        // GET: ServiceOrderController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ServiceOrderController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ServiceOrderController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ServiceOrderController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ServiceOrderController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ServiceOrderController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
