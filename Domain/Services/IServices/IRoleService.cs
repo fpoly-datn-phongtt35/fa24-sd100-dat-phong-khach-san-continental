40bc245ca91ce25e653f044d1fe40320841b0e06
Nguyen Hong Hai <hainhph35304@fpt.edu.vn>
2024-10-01 15:30:39 +0700

    CRUD Role

---
+++ Domain/Services/IServices/IRoleService.cs
@@ -0,0 +1,15 @@
+﻿using Domain.DTO.Paging;
+using Domain.DTO.Role;
+using Domain.Models;
+
+namespace Domain.Services.IServices
+{
+    public interface IRoleService
+    {
+        Task<int> AddRole(RoleCreateRequest request);
+        Task<int> UpdateRole(RoleUpdateRequest request);
+        Task<int> DeleteRole(RoleDeleteRequest request);
+        Task<ResponseData<Role>> GetAllRole(RoleGetRequest Role);
+        Task<Role> GetRoleById(Guid Id);
+    }
+}
