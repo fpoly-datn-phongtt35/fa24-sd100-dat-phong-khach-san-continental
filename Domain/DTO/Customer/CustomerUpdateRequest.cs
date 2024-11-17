2d119d5eb26ca733b7d94d652efb4890ac2842f6
Nguyen Hong Hai <hainhph35304@fpt.edu.vn>
2024-11-18 02:53:51 +0700

    fixbug customer

--- Domain/DTO/Customer/CustomerUpdateRequest.cs
+++ Domain/DTO/Customer/CustomerUpdateRequest.cs
@@ -9,20 +9,14 @@ namespace Domain.DTO.Customer
         [Required]
         [RegularExpression(@"^[a-zA-Z0-9]{3,12}$", ErrorMessage = "Tên người dùng phải từ 3-12 kí tự, không bao gồm kí tự đặc biệt!")]
         public string UserName { get; set; } = string.Empty;
-        [Required]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$", ErrorMessage = "Mật khẩu gồm chữ viết hoa, chữ viết thường, chữ số và chỉ từ 8-15 kí tự!")]
-        public string Password { get; set; } = string.Empty;
-        [RegularExpression(@"^[\p{L}\s]{3,50}$", ErrorMessage = "Vui lòng nhập tên chỉ chứa chữ!")]
         public string FirstName { get; set; } = string.Empty;
-
-        [RegularExpression(@"^[\p{L}\s]{3,50}$", ErrorMessage = "Vui lòng nhập tên chỉ chứa chữ!")]
         public string LastName { get; set; } = string.Empty;
         [Required]
         [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Vui lòng nhập đúng định dạng: example@domain.com")]
         public string Email { get; set; } = string.Empty;
         [RegularExpression(@"^(\+?\d{1,2}[- ]?)?\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}$", ErrorMessage = "Vui lòng nhập số điện thoại hợp lệ!")]
         public string PhoneNumber { get; set; } = string.Empty;
-        public int? Gender { get; set; }
+        public GenderType? Gender { get; set; }
 
         public DateTime? DateOfBirth { get; set; }
         public EntityStatus? Status { get; set; } = EntityStatus.Active;
