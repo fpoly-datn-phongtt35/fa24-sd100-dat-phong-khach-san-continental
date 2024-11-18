7ff809de18b60f1e7fe238b747d86b68851d218e
Nguyen Hong Hai <hainhph35304@fpt.edu.vn>
2024-11-18 22:33:14 +0700

    push sth

---
+++ ViewClient/Controllers/RoomBookingController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using ViewClient.Repositories.IRepository;
+using ViewClient.Repositories.Repository;
+
+namespace ViewClient.Controllers
+{
+    public class RoomBookingController : Controller
+    {
+        private readonly IRoombooking _roomBookingRepo;
+        private readonly HttpClient _httpClient;
+        public RoomBookingController(IRoombooking roomBookingRepo, HttpClient httpClient)
+        {
+            _roomBookingRepo = roomBookingRepo;
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://localhost:7130/");
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index()
+        {
+            var _UserLogin = Guid.Empty;
+            if (HttpContext.User.FindFirst(ClaimTypes.NameIdentifier) != null)
+            {
+                _UserLogin = Guid.Parse(HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
+                if (_UserLogin == Guid.Empty) 
+                {
+                    
+                }
+            }
+            
+            return View();
+        }
+      
+    }
+}
