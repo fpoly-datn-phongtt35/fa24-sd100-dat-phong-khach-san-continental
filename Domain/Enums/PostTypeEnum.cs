5f77ebff2442ac698bccbe1f55423e4cd3fa5a1a
Vũ Tuấn Anh <tuananh26ttvt@gmail.com>
2025-01-08 17:03:43 +0700

    sua Post

---
+++ Domain/Enums/PostTypeEnum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum PostTypeEnum
+    {
+        General = 0,            
+        PrivacyPolicy = 1,    
+        RefundPolicy = 2,      
+        UsagePolicy = 3,        
+        ServiceAgreement = 4, 
+        PaymentPolicy = 5,     
+        CancellationPolicy = 6, 
+        Other = 99
+    }
+}
