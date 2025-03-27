2030cda1ee814ca57d8ff72c2b586e49b17a52a4
IamBemooooo <nguyendinhthe405@gmail.com>
2025-03-27 15:34:05 +0700

    Fix UI + Module Feedback

---
+++ ViewClient/Controllers/FeedbackController.cs
@@ -0,0 +1,36 @@
+﻿using Domain.DTO.Feedback;
+using Domain.DTO.Paging;
+using Domain.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ViewClient.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        private readonly IFeedbackService _feedbackService;
+
+        public FeedbackController(IFeedbackService feedbackService)
+        {
+            _feedbackService = feedbackService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetListFeedbacks(FeedbackGetRequest request)
+        {
+            var lst = new ResponseData<FeedbackDto>();
+            try
+            {
+                lst = await _feedbackService.GetListFeedback(request);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return View(lst);
+        }
+    }
+}
