43e6085b33c2191b86d152aa060f140b73969ba5
IamBemooooo <nguyendinhthe405@gmail.com>
2025-03-28 18:01:35 +0700

    Enforce module Feeback

---
+++ ViewClient/Repositories/IRepository/IFeedback.cs
@@ -0,0 +1,10 @@
+﻿using Domain.DTO.Feedback;
+using Domain.DTO.Paging;
+
+namespace ViewClient.Repositories.IRepository
+{
+    public interface IFeedback
+    {
+        Task<ResponseData<FeedbackDto>> GetListFeedbacks(FeedbackGetRequest request);
+    }
+}
---
+++ ViewClient/Repositories/Repository/Feedback.cs
@@ -0,0 +1,27 @@
+﻿using Domain.DTO.Feedback;
+using Domain.DTO.Paging;
+using ViewClient.Repositories.IRepository;
+
+namespace ViewClient.Repositories.Repository
+{
+    public class Feedback : IFeedback
+    {
+        private readonly HttpClient _httpClient;
+
+        public Feedback(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ResponseData<FeedbackDto>> GetListFeedbacks(FeedbackGetRequest request)
+        {
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync("https://localhost:7130/api/Feedback/GetFeedbacks", request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<ResponseData<FeedbackDto>>();
+            }
+            return null;
+        }
+    }
+}
